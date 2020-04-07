87c95d6b78f2183e300ef9900caddaac556caddc
Batman <57849598+eat-sleep-code-repeat-it@users.noreply.github.com>
2020-04-06 23:17:27 -0400

    cov19 charts with ngx-charts

--- projects/covid19/README.md
+++ projects/covid19/README.md
@@ -1,24 +1,13 @@
 # Covid19
 
-This library was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.7.
+## [2019-nCoV Data by Johns Hopkins CSSE](https://github.com/CSSEGISandData/COVID-19)
 
-## Code scaffolding
+## [D3.js — Data-Driven Documents](https://d3js.org/)
 
-Run `ng generate component component-name --project covid19` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module --project covid19`.
-> Note: Don't forget to add `--project covid19` or else it will be added to the default project in your `angular.json` file. 
+## [Google Charts](https://google-developers.appspot.com/chart/)
 
-## Build
+## [ng2-charts](https://www.npmjs.com/package/ng2-charts)
 
-Run `ng build covid19` to build the project. The build artifacts will be stored in the `dist/` directory.
+- https://www.positronx.io/angular-chart-js-tutorial-with-ng2-charts-examples/
 
-## Publishing
-
-After building your library with `ng build covid19`, go to the dist folder `cd dist/covid19` and run `npm publish`.
-
-## Running unit tests
-
-Run `ng test covid19` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+## [n3-charts](http://n3-charts.github.io/line-chart/#/home)
--- projects/covid19/src/lib/covid19.component.ts
+++ projects/covid19/src/lib/covid19.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Covid19Service } from './covid19.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'lib-covid19',
@@ -7,31 +8,31 @@ import { Covid19Service } from './covid19.service';
   styles: []
 })
 export class Covid19Component implements OnInit {
-  confirmedView: any[] = [1200, 200];
+  confirmedView: any[] = [1200, 250];
   confirmedSeriesData: any[];
   confirmedColorScheme = {
     domain: ['#7aa3e5', '#a8385d', '#aae3f5']
   };
 
-  deathView: any[] = [1200, 200];
+  deathView: any[] = [1200, 250];
   deathSeriesData: any[];
   deathColorScheme = {
     domain: ['#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
   };
 
-  recoveredView: any[] = [1200, 200];
+  recoveredView: any[] = [1200, 250];
   recoveredSeriesData: any[];
   recoveredColorScheme = {
     domain: ['#5AA454','#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
   };
   // options
-  legend: boolean = true;
+  legend: boolean = false;
   showLabels: boolean = true;
   animations: boolean = true;
   xAxis: boolean = true;
   yAxis: boolean = true;
   showYAxisLabel: boolean = true;
-  showXAxisLabel: boolean = true;
+  showXAxisLabel: boolean = false;
   xAxisLabel: string = 'Day';
   yAxisLabel: string = 'Cases';
   timeline: boolean = true;
@@ -39,11 +40,27 @@ export class Covid19Component implements OnInit {
   colorScheme = {
     domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
   };
-  constructor(private covidService: Covid19Service) {
-    
-  }
+  covSeriesData = [];
+
+  constructor(private covidService: Covid19Service) { }
 
   ngOnInit(): void {
+    forkJoin(
+      this.covidService.getCovidConfirmedTimeSerie(),
+      this.covidService.getCovidDeathTimeSerie(),
+      this.covidService.getCovidRecoveredTimeSerie()
+    ).subscribe(([confirmed, death, recovered]) => {
+      const data = [].concat(confirmed, death, recovered);
+
+      // covert to json object
+      /*
+        Object.assign({}, data);
+        this.covSeriesData = data;
+      */
+      this.covSeriesData = JSON.parse(JSON.stringify(data));
+    });
+
+
     this.covidService.getCovidConfirmedTimeSerie().subscribe(data => {
       this.confirmedSeriesData = data;
       Object.assign(this, { data });
